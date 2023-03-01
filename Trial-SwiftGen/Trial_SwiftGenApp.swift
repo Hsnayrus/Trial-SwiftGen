7ac9fef73ae9ad6ea4b55adc99ffa6b24f5e5f53
Hsnayrus <your_email@gmail.com>
2023-02-28 20:35:09 -0500

    Initial Commit

---
+++ Trial-SwiftGen/Trial_SwiftGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trial_SwiftGenApp.swift
+//  Trial-SwiftGen
+//
+//  Created by loaner on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Trial_SwiftGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
